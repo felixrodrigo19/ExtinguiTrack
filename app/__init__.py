128df8cf5f84b08caad2205529bd9bb4908ceca0
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-09-03 18:39:13 -0300

    feat: initial auth class

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_restful import Api
 
-from app.ext import config
+from app import config
+from app.resources.auth import Auth
 
 
 def create_app(config_filename):
     app = Flask(__name__, instance_relative_config=True)
+    api = Api(app)
     config.init_app(app, config_filename)
+    api.add_resource(Auth, '/auth')
 
     return app
