8993e809ca25442e705d30d01578cdc38575f8af
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-10-30 19:07:17 -0300

    feat: add created_at

--- app/models.py
+++ app/models.py
@@ -36,6 +36,7 @@ class FireExtinguisher(models.Model):
     serial_number = models.CharField(max_length=20)
     manufacturing_date = models.DateField()
     last_inspection_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
