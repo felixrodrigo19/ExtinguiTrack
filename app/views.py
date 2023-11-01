12d7b552ab045b7c72551dd6cef474ae8772e4fb
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-10-31 21:27:03 -0300

    docs: add views titles

--- app/views.py
+++ app/views.py
@@ -17,35 +17,56 @@ from app.serializers import FireExtinguisherSerializer
 
 
 class FireExtinguishersViewSet(viewsets.ModelViewSet):
+    """
+    Returns a list of all **Extinguishers** registered in the system.
+    """
     serializer_class = FireExtinguisherSerializer
     queryset = FireExtinguisher.objects.all()
 
 
 class TypeViewSet(viewsets.ModelViewSet):
+    """
+    Returns a list of all **Extinguishers Types** registered in the system.
+    """
     serializer_class = TypeSerializer
     queryset = Type.objects.all()
 
 
 class CapacityViewSet(viewsets.ModelViewSet):
+    """
+    Returns a list of all **Extinguishers Capacity** registered in the system.
+    """
     serializer_class = CapacitySerializer
     queryset = Capacity.objects.all()
 
 
 class AgentViewSet(viewsets.ModelViewSet):
+    """
+    Returns a list of all **Extinguishers Agent Types** registered in the system.
+    """
     serializer_class = AgentSerializer
     queryset = Agent.objects.all()
 
 
 class LocationViewSet(viewsets.ModelViewSet):
+    """
+    Returns a list of all **Extinguishers Location** registered in the system.
+    """
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
 
 
 class SupplierViewSet(viewsets.ModelViewSet):
+    """
+    Returns a list of all **Suppliers** registered in the system.
+    """
     serializer_class = SupplierSerializer
     queryset = Supplier.objects.all()
 
 
 class ManufactureViewSet(viewsets.ModelViewSet):
+    """
+    Returns a list of all **Manufactures** registered in the system.
+    """
     serializer_class = ManufactureSerializer
     queryset = Manufacture.objects.all()
