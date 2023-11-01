7947f006e751cb479f06b37e55967a05588a06eb
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-10-31 21:09:00 -0300

    refactor: extract app routes to app package

--- setup/urls.py
+++ setup/urls.py
@@ -1,22 +1,6 @@
 from django.urls import path, include
-from rest_framework import routers
 
-from app.views import FireExtinguishersViewSet
-from app.views import TypeViewSet
-from app.views import LocationViewSet
-from app.views import SupplierViewSet
-from app.views import ManufactureViewSet
-from app.views import AgentViewSet
-from app.views import CapacityViewSet
-
-router = routers.DefaultRouter()
-router.register(r'extinguishers', FireExtinguishersViewSet)
-router.register(r'types', TypeViewSet)
-router.register(r'location', LocationViewSet)
-router.register(r'manufacture', ManufactureViewSet)
-router.register(r'agents', AgentViewSet)
-router.register(r'capacity', CapacityViewSet)
-router.register(r'suppliers', SupplierViewSet)
+from app.urls import router
 
 urlpatterns = [
     path('', include(router.urls)),
