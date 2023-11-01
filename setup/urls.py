aabbd71949267df0ae4c73f7dec8643ff51dbd04
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-11-01 08:42:15 -0300

    feat: setup token auth

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
 from app.urls import router
+from app.views import CustomAuthToken
 
 urlpatterns = [
     path('', include(router.urls)),
+    path(r'api-token-auth', CustomAuthToken.as_view())
 ]
